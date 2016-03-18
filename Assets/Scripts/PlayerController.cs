dcdd84af1c89d70ce9dae1a70acb76ab49294f77
Danny Tizon <dtizon@gmail.com>
2016-03-17 19:45:40 -0700

    camera setup

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public Rigidbody rb;
+	public float speed = 250;
+
+	private float speedScale;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+		speedScale = speed * Time.deltaTime;
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+		rb.AddForce (movement * speedScale);
+	}
+
+}
